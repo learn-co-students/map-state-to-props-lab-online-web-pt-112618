5fc7d72f2f3905b0cbd41d58d553ace1ad851070
Whitny Edwards <wne4e2@gmail.com>
2019-10-23 20:50:05 -0400

    Done.

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,18 +1,51 @@
+// import React, { Component } from 'react';
+// import { connect } from 'react-redux'
+
+
+// class Users extends Component {
+
+//   render() {
+//     return (
+//       <div>
+//         <ul>
+//           Users!
+//         </ul>
+//       </div>
+//     )
+//   }
+// }
+
+//add mapStateToProps here
+
+// export default Users
+
+
+// export default connect ( state => ({ }))(Users)
+
+
+
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Users extends Component {
 
   render() {
     return (
       <div>
+         <p>Users: {this.props.users.length}</p>
         <ul>
-          Users!
+          {this.props.users !== [] ?
+          this.props.users.map(user => (
+            <li>{user.username}<br></br>{user.hometown}</li>
+          )) : null }
         </ul>
       </div>
     )
   }
 }
 
-//add mapStateToProps here
+const mapStateToProps = (state) => {
+  return {users: state.users}
+}
 
-export default Users
+export default connect(mapStateToProps)(Users);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
-
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import manageUsers from './reducers/manageUsers'
+//combined reducers allows
+//reducers don't get imported, because of the store/which is the global state 
+//mapStatetoProps connects to store 
+//mapDispatch has to do with actions
+//Provider wraps app/store, allows react components to access mapStateToProps
 
-
-// add imports and code
-
+//createStore accepts three arguments 
+//most important is the reducer 
+const store = createStore(
+  manageUsers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+ 
 
 ReactDOM.render(
-  // add imports and code
-  <App />
-  // add imports and code
-  ,
+  <Provider store={store}>
+    <App store={store}/>
+  </Provider>,
   document.getElementById('root')
 );
+
+
+
+//createStore method --> always
+//dispatch method is used to dispatch actions and update the state
+
+
+//deconstructor { } means creating variable: keys are Provider and createStore
+//Redux has function called Create Store that I am accessing 
+//Provider is a component 
